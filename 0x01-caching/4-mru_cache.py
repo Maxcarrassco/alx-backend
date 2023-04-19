16117fccd01d6955016f797730b1eda3cd4262d1
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-19 15:22:26 +0000

    Created a script that implements a caching system using MRU

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""ALX SE Backend Caching Module."""
+from base_caching import BaseCaching
+from typing import Union, Any
+from collections import deque
+
+
+class MRUCache(BaseCaching):
+    """This model implement a caching system using MRU."""
+
+    def __init__(self) -> None:
+        """Initialize the model."""
+        super().__init__()
+        self.queue = deque()
+
+    def put(self, key, value) -> None:
+        """Put a value in the cache."""
+        if not key or not value:
+            return
+        if len(self.cache_data) < self.MAX_ITEMS or key in self.cache_data:
+            if key not in self.cache_data:
+                self.queue.append(key)
+            self.cache_data[key] = value
+        else:
+            k = self.queue.pop()
+            del self.cache_data[k]
+            print('DISCARD:', k)
+            self.cache_data[key] = value
+            self.queue.append(key)
+
+    def get(self, key) -> Union[Any, None]:
+        """Get a value from the cache."""
+        val = self.cache_data.get(key)
+        if val and key in self.queue:
+            self.queue.remove(key)
+            self.queue.append(key)
+        return val
