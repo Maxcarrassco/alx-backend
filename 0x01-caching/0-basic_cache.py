7feb9da124026ac29d12fd9f18a63416a152c9df
Maxwell D. Dorliea <deboy170@gmail.com>
2023-04-19 10:44:23 +0000

    Created a script that implement a basic caching system

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""ALX SE Backend Caching Module."""
+from base_caching import BaseCaching
+from typing import Union, Any
+
+
+class BasicCache(BaseCaching):
+    """This model implement a basic caching system."""
+
+    def put(self, key, value) -> None:
+        """Put a value in the cache."""
+        self.cache_data[key] = value
+
+    def get(self, key) -> Union[Any, None]:
+        """Get a value from the cache."""
+        return self.cache_data.get(key)
